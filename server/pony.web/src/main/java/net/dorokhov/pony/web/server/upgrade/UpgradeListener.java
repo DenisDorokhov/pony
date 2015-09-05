f7d70d2eee150c755ff40e52a180685f9f8f0286
DenisDorokhov <denis@dorokhov.net>
2015-09-05 21:55:46 +0200

    refactoring (move default properties to core), upgrade servlet listener

---
+++ server/pony.web/src/main/java/net/dorokhov/pony/web/server/upgrade/UpgradeListener.java
@@ -0,0 +1,52 @@
+package net.dorokhov.pony.web.server.upgrade;
+
+import net.dorokhov.pony.core.upgrade.UpgradeService;
+import net.dorokhov.pony.core.version.VersionProvider;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.io.ByteArrayResource;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.io.InputStream;
+
+public class UpgradeListener implements ServletContextListener {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public void contextInitialized(ServletContextEvent aServletContext) {
+
+		InputStream xmlStream = aServletContext.getServletContext().getResourceAsStream("/WEB-INF/context/upgrade.xml");
+
+		ConfigurableApplicationContext context = null;
+
+		try {
+			context = new GenericXmlApplicationContext(new ByteArrayResource(IOUtils.toByteArray(xmlStream)));
+		} catch (Exception e) {
+			log.error("Could not initialize application context.", e);
+		} finally {
+			IOUtils.closeQuietly(xmlStream);
+		}
+
+		if (context != null) {
+			try {
+
+				VersionProvider versionProvider = context.getBean(VersionProvider.class);
+				UpgradeService upgradeService = context.getBean(UpgradeService.class);
+
+				upgradeService.upgrade(versionProvider.getVersion());
+
+			} finally {
+				context.close();
+			}
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent aServletContext) {}
+
+}
